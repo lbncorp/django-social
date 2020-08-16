91e22066f0e2e55c29f5befa53d092603b37dec0
Umaru Lamin <ulamin97@gmail.com>
2020-08-15 20:51:06 -0400

    umaru commit

---
+++ umaru.py
@@ -0,0 +1,16 @@
+
+import json
+
+
+# JSON string
+a = '{"name": "Bob", "languages": "English", "subject": "Math"}'
+
+# deserializes into dict
+# and returns dict.
+y = json.loads(a)
+
+print("JSON string = ", y)
+print()
+
+
+
